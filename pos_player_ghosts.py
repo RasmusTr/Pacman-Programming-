906899401c14009194d8043ab7c9a925f9b24e99
Rasmus Trommer <rasmustrommer@web.de>
2025-11-30 18:01:03 +0100

    Update pos_player_ghosts.py

--- pos_player_ghosts.py
+++ pos_player_ghosts.py
@@ -1,109 +1,107 @@
 # pos_player_ghosts.py
 #------------------------------------------------------------------------------ 
-# importiere Module 
+# import modules 
 import random
 import pygame
 from typing import List, Tuple
 
-# Konstante Informationen fuer die Funktionen
-# Groesse der Pixel der Sprites
+# Constant information for the functions
+# Size of the sprite pixels
 CELL_SIZE = 16 
 
-# Funktion Berechnung Starposition der  Geister
+# Function to calculate the starting position of ghosts
 def pos_geister(
     number_ghosts: int,
     labyrinth: List,
 ) -> Tuple[List]:
     """
-    @param number_ghots: Anzahl der Geister
-    @param labyrinth: generierte Zufallslabyrinth aus create_labyrinth.py generiert in Mainfunction
+    @param number_ghosts: Number of ghosts
+    @param labyrinth: Generated random labyrinth from create_labyrinth.py in main function
     return:
-    list_ghost: Liste der Geister, enthaelt Starposition, Bewegungsrichtung, Imgae, slowness, counter
+    list_ghost: List of ghosts containing start position, movement direction, image, slowness, counter
     """
   
-    # list_ghost ist Liste welche Startpunkte der Geister, sowie deren Startposition enthält 
+    # list_ghosts is the list containing ghost start points and their start position 
     list_ghosts =[]
   
-    # die 1en und 2en kommen daher, dass das Labyrinth, welches als Liste uebergeben
-    # wird, mit Waenden umrandet ist und daher 0,0 und len(labyrinth) einen Baustein zu groß sind
-    # Quadratische Labyrinth vier Ecken --> Berechnung der vier Eckkoordinaten
-    # multiplizieren mit CELL_SIZE fuer die Startposition in Pixeln
+    # The 1s and 2s are because the labyrinth, passed as a list, is bordered with walls 
+    # so 0,0 and len(labyrinth) are one unit too large
+    # Square labyrinth corners --> calculate four corner coordinates
+    # Multiply by CELL_SIZE for start position in pixels
   
-    # x, y Koordinaten Ecke oben rechts fuer Startpunkt der Geister
+    # x, y coordinates top-right corner for ghost start
     x_pos_corner1 = (len(labyrinth) - 2) * CELL_SIZE
     y_pos_corner1 = 1 * CELL_SIZE
-    # x, y Koordinaten Ecke oben links fuer Startpunkt der Geister
+    # x, y coordinates top-left corner for ghost start
     x_pos_corner2 = 1 * CELL_SIZE
     y_pos_corner2 = 1 * CELL_SIZE
-    # x, y Koordinaten Ecke unten rechts fuer Startpunkt der Geister
+    # x, y coordinates bottom-right corner for ghost start
     x_pos_corner3 = (len(labyrinth) - 2) * CELL_SIZE
     y_pos_corner3 = (len(labyrinth) - 2) * CELL_SIZE
-    # x, y Koordinaten Ecke untenlinks fuer Startpunkt der Geister
+    # x, y coordinates bottom-left corner for ghost start
     x_pos_corner4 = 1 * CELL_SIZE
     y_pos_corner4 = (len(labyrinth) - 2) * CELL_SIZE
 
-    # waehlen einer zufaelligen Startrichtung 
+    # choose a random starting direction 
     start_direction = random.choice(["up", "down", "left", "right" ])
 
-    # Liste Namen/Dateipfad der Bilddateien 
+    # List of ghost image file paths
     PIC_GHOSTS = ["bilder/ghosts/blinky.png", "bilder/ghosts/clyde.png", "bilder/ghosts/inky.png", "bilder/ghosts/pinky.png"]
-    counter = 0 # Counter fuer Verlangsamung --> Anfangs 0 
-    slowness = False  # slowness auf False, da noch kein Spieler-Geist-Kontakt vorliegt 
+    counter = 0 # Counter for slowness --> initially 0 
+    slowness = False  # Slowness set to False, no player-ghost contact yet 
 
-    # while-Schleife --> Verteilung der Geister gleichmaessig auf 4 Ecken bis alle verteilt sind 
+    # while loop --> evenly distribute ghosts across 4 corners until all assigned
     while number_ghosts > 0:
 
-        if number_ghosts != 0:  # Zuweisung Geist Ecke, uebergabe der Koordinaten, Startrichtung, Sprite, slowness und counter 
+        if number_ghosts != 0:  # Assign ghost to corner, pass coordinates, start direction, sprite, slowness, counter 
             list1 = [x_pos_corner1, y_pos_corner1, random.choice(["up", "down", "left", "right" ]), pygame.image.load(random.choice(PIC_GHOSTS)), slowness, counter ]
             list_ghosts += [list1.copy()]
-            number_ghosts -= 1  # Abziehen von Anzahl nach Zuweisung 
+            number_ghosts -= 1  # Decrease number after assignment 
       
-        if number_ghosts != 0:  # Zuweisung Geist Ecke, uebergabe der Koordinaten, Startrichtung, Sprite, slowness und counter 
+        if number_ghosts != 0:
             list2 = [x_pos_corner2, y_pos_corner2, random.choice(["up", "down", "left", "right" ]), pygame.image.load(random.choice(PIC_GHOSTS)), slowness, counter]
             list_ghosts += [list2.copy()]
             number_ghosts -= 1
-      
-        if number_ghosts != 0:  # Zuweisung Geist Ecke, uebergabe der Koordinaten, Startrichtung, Sprite, slowness und counter 
+          
+        if number_ghosts != 0:
             list3 = [x_pos_corner3, y_pos_corner3, random.choice(["up", "down", "left", "right" ]), pygame.image.load(random.choice(PIC_GHOSTS)), slowness, counter ]
             list_ghosts += [list3.copy()]
-            number_ghosts -= 1  # Abziehen von Anzahl nach Zuweisung 
-      
-        if number_ghosts != 0:  # Zuweisung Geist Ecke, uebergabe der Koordinaten, Startrichtung, Sprite, slowness und counter 
+            number_ghosts -= 1
+          
+        if number_ghosts != 0:
             list4 = [x_pos_corner4, y_pos_corner4, random.choice(["up", "down", "left", "right" ]), pygame.image.load(random.choice(PIC_GHOSTS)), slowness, counter ]
             list_ghosts += [list4.copy()]
-            number_ghosts -= 1  # Abziehen von Anzahl nach Zuweisung 
+            number_ghosts -= 1
 
-    # Rueckgabe der Liste 
+    # Return the list 
     return(list_ghosts)
 
 
-# Funktion Berechung Startposition Spieler
+# Function to calculate player starting position
 def pos_player(
   labyrinth: List
 ) -> Tuple[int, int]:
     """
-    @param labyrinth: generierte Zufallslabyrinth aus create_labyrinth.py generiert in Mainfunction
+    @param labyrinth: Generated random labyrinth from create_labyrinth.py in main function
     returns:
-    player_pos_x, player_pos_y: Koordinaten des Spielers (Pixel)
+    player_pos_x, player_pos_y: Player coordinates (pixels)
     """
 
-    zaehler = 0
-    # Bedigung, wenn Feld gefunden: False 
+    counter = 0
+    # Condition, when field found: False 
     field_find = True 
 
     while field_find:
 
-        # Zaehler fuer Erhoehung Position nach "rechts" in Liste 
-        zaehler += 1
-        # Durchlaufen Zeile nach "rechts" mit Zaehler ausgehend von Mitte des Labyrinths   
-        if labyrinth[len(labyrinth)//2][len(labyrinth)//2 + zaehler] == " ": # Freies Feld ' ' gefunden 
-            field_find = False  # Bedingung False 
+        # Counter to increase position to the "right" in the list 
+        counter += 1
+        # Traverse row to the right from labyrinth center
+        if labyrinth[len(labyrinth)//2][len(labyrinth)//2 + counter] == " ": # Free field ' ' found 
+            field_find = False  # Condition False 
 
-    # Skalieren Position auf Koordinaten auf dem Bildschirm: multiplikation mit cell_size
-    player_pos_x = (len(labyrinth)//2 + zaehler) * CELL_SIZE
+    # Scale position to screen coordinates: multiply by CELL_SIZE
+    player_pos_x = (len(labyrinth)//2 + counter) * CELL_SIZE
     player_pos_y = (len(labyrinth)//2) * CELL_SIZE
 
-    # Rueckgabe der Koordinaten  
+    # Return coordinates  
     return player_pos_x, player_pos_y
-
-    
